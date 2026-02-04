2c5c108d0e45aecffb074387f20a65b2558e91d4
ken kagai <jibken80@gmail.com>
2026-02-04 15:50:16 +0300

    Refactor Member admin registration to enhance display, filtering, and search capabilities

--- backend/members/admin.py
+++ backend/members/admin.py
@@ -1,6 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Member
 
-# Register your models here.
 
-admin.site.register(Member)
+@admin.register(Member)
+class MemberAdmin(UserAdmin):
+    model = Member
+
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'district',
+        'is_super_admin',
+        'is_parish_minister',
+        'is_kirk_session',
+        'can_approve_pending',
+        'is_active',
+    )
+
+    list_filter = (
+        'district',
+        'is_super_admin',
+        'is_parish_minister',
+        'is_kirk_session',
+        'can_approve_pending',
+        'is_active',
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Church Roles', {
+            'fields': (
+                'phone_number',
+                'district',
+                'is_super_admin',
+                'is_parish_minister',
+                'is_kirk_session',
+                'can_approve_pending',
+            )
+        }),
+    )
+
+    search_fields = ('username', 'first_name', 'last_name', 'phone_number')
