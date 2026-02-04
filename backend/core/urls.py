60339aa702ab7edb6fd243cc57e92e88f1e11750
ken kagai <jibken80@gmail.com>
2026-02-04 17:06:06 +0300

    Add URL routing for activities API

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/', include('members.urls')),
+    path('api/', include('activities.urls')),
 ]
