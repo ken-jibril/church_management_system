8a7af56119b716a8260df6e1558a8fb11be260e6
ken kagai <jibken80@gmail.com>
2026-02-05 20:07:08 +0300

    Implement routing with AppRouter and add Login, Register, and Dashboard pages

--- frontend/core/src/App.jsx
+++ frontend/core/src/App.jsx
@@ -1,10 +1,10 @@
-
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   
   return (
     <>
-     <h1 className="text-3xl sm:text-2xl md:text-3xl font-bold">Welcome To your Church Management System</h1>
+     <AppRouter />
     </>
   )
 }
