1968dad1b943f7b8d47515b7fcb4eaf1816ab90d
ken kagai <jibken80@gmail.com>
2026-02-06 11:26:11 +0300

    Add os import and configure ALLOWED_HOSTS from environment variables

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-+0dm24h7s6v%)3389d*a@m3hbeo^*x$%2^q4mi#w#v9^$4qf%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
