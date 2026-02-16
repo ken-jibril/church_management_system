e44bbc579cfa33b533cd75c6fe999efbd50cd690
ken kagai <jibken80@gmail.com>
2026-02-16 14:19:36 +0300

    update the urls of members app for making requests.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/', include('members.urls')),
     path('api/activities/', include('activities.urls')),
