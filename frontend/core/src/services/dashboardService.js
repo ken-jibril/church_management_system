24a6d7adb8bae0525559f8f7fc1dd4c8943c4812
ken kagai <jibken80@gmail.com>
2026-02-17 17:34:28 +0300

    Refactor urls being used for making requests to the server.

--- frontend/core/src/services/dashboardService.js
+++ frontend/core/src/services/dashboardService.js
@@ -1,12 +1,12 @@
 import api from "../api/axios.js";
 
 export const getMembers = async () => {
-  const res = await api.get("/members/");
+  const res = await api.get("/members/all/");
   return res.data;
 };
 
 export const getActivities = async () => {
-  const res = await api.get("/activities/");
+  const res = await api.get("/events/");
   return res.data;
 };
 
