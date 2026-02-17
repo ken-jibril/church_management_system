440f0e3fddce56a923d3a0b45d2c621374950d70
ken kagai <jibken80@gmail.com>
2026-02-17 17:32:06 +0300

    Import member view set, and render the right endpoint for rendering and displaying all members.

--- backend/members/urls.py
+++ backend/members/urls.py
@@ -1,10 +1,11 @@
 # members/urls.py
 from rest_framework.routers import DefaultRouter
-from .views import MemberRegistrationViewSet
+from .views import MemberRegistrationViewSet, MemberViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'members', MemberRegistrationViewSet, basename='members')
+router.register(r'all', MemberViewSet, basename='all-members')
 
 urlpatterns = [
     path("", include(router.urls)),
