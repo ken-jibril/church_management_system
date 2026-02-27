50fdec25571544d74251565d29d7cc20b1fd9d35
ken kagai <jibken80@gmail.com>
2026-02-27 20:38:21 +0300

    fix(auth): Fix password hashing in MemberSerializer

--- backend/members/serializers.py
+++ backend/members/serializers.py
@@ -3,6 +3,25 @@ from rest_framework import serializers
 from .models import Member
 
 class MemberSerializer(serializers.ModelSerializer):
+    # Override password field to write-only
+    password = serializers.CharField(write_only=True, required=False)
+    
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'email', 
+                  'phone_number', 'is_super_admin', 'is_parish_minister', 'is_kirk_session',
+                  'can_approve_pending', 'district', 'created_at']
+        
+    def create(self, validated_data):
+        # Remove password from validated data
+        password = validated_data.pop('password', None)
+        
+        # Create user instance
+        user = Member(**validated_data)
+        
+        # Hash password if provided
+        if password:
+            user.set_password(password)
+        
+        user.save()
+        return user
